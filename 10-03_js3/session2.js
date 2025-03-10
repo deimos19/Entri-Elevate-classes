c9f99f1d8cac498423334f353cd381a015ae49d0
deimos19 <devadarshm2001@gmail.com>
2025-03-10 22:00:39 +0530

    10-03

---
+++ 10-03_js3/session2.js
@@ -0,0 +1,17 @@
+var name = "E62"
+
+function sum(){
+    var name = "E62 batch"
+    console.log(name)
+}
+
+sum()
+
+console.log(name)
+
+//object
+let student = {
+    name: "deva",
+    age: 23,
+    place: "kollam"
+}
---
+++ 10-03_js3/session3.js
@@ -0,0 +1,4 @@
+let a = 2;
+console.log(a == 2);
+
+console.log(1 == true);
